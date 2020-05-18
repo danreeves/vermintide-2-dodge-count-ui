192861edfbbd4cdbb8cd06765a3dd70f4c9a968f
Dan Reeves <hey@danreev.es>
2020-05-18 20:22:08 +0100

    move to own repo

---
+++ scripts/mods/dodge-count-ui/dodge-count-ui_localization.lua
@@ -0,0 +1,35 @@
+return {
+  mod_description = {
+    en = "Display your dodge count, number of effective dodges, and dodge cooldown",
+  },
+  always_on = {
+    en = "Always visible",
+  },
+  always_on_description = {
+    en = "When disabled the UI will disappear when your dodge count is full",
+  },
+  offset_x = {
+    en = "Offset X",
+  },
+  offset_x_description = {
+    en = "Offset from the center of the screen in the X axis",
+  },
+  offset_y = {
+    en = "Offset Y",
+  },
+  offset_y_description = {
+    en = "Offset from the center of the screen in the Y axis",
+  },
+  dodge_count_font_size = {
+    en = "Dodge count font size"
+  },
+  dodge_count_font_size_description = {
+    en = "Dodge count font size"
+  },
+  cooldown_font_size = {
+    en = "Cooldown font size"
+  },
+  cooldown_font_size_description = {
+    en = "Cooldown font size"
+  },
+}
